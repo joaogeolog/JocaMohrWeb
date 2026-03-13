eff71ccaee75fc2a413d4da01587f632e282b2c6
joaogeolog <joaogeolog@gmail.com>
2026-03-13 13:07:39 -0300

    Update visualizacao_plots.py

--- visualizacao_plots.py
+++ visualizacao_plots.py
@@ -5,21 +5,27 @@ import numpy as np
 def plot_mohr(x_env, y_env, xt_coll, xc_s, yc_s, xc_f, yc_f, env_high, sn_p, tn_p, p_x, p_y, falhou, params):
     with st.container(border=True):
         fig = go.Figure()
+        
         fig.add_shape(type="line", x0=-50, y0=0, x1=250, y1=0, line=dict(color="black", width=2))
         fig.add_shape(type="line", x0=0, y0=0, x1=0, y1=100, line=dict(color="black", width=2))
         
-        m_tra, m_cis, m_col = x_env <= 0, (x_env > 0) & (x_env <= xt_coll), x_env > xt_coll
-        fig.add_trace(go.Scatter(x=x_env[m_tra], y=y_env[m_tra], line=dict(color='blue', width=2.5), name="Tração"))
-        fig.add_trace(go.Scatter(x=x_env[m_cis], y=y_env[m_cis], line=dict(color='red', width=2.5), name="Cisalhamento"))
-        fig.add_trace(go.Scatter(x=x_env[m_col], y=y_env[m_col], line=dict(color='green', width=2.5), name="Colapso"))
+        m_tra = x_env <= 0
+        m_cis = (x_env > 0) & (x_env <= xt_coll)
+        m_col = x_env > xt_coll
+        
+        fig.add_trace(go.Scatter(x=x_env[m_tra], y=y_env[m_tra], line=dict(color='blue', width=2.5), name="Ruptura por tração"))
+        fig.add_trace(go.Scatter(x=x_env[m_cis], y=y_env[m_cis], line=dict(color='red', width=2.5), name="Ruptura por cisalhamento"))
+        fig.add_trace(go.Scatter(x=x_env[m_col], y=y_env[m_col], line=dict(color='green', width=2.5), name="Colapso de poros"))
         
         try:
             if env_high is not None and any(env_high):
-                fig.add_trace(go.Scatter(x=x_env[env_high], y=y_env[env_high], line=dict(color='yellow', width=5, opacity=0.3), showlegend=False, hoverinfo='skip'))
+                x_h, y_h = x_env[env_high], y_env[env_high]
+                if len(x_h) > 0:
+                    fig.add_trace(go.Scatter(x=x_h, y=y_h, line=dict(color='yellow', width=5, opacity=0.3), showlegend=False, hoverinfo='skip'))
         except: pass
 
-        fig.add_trace(go.Scatter(x=xc_s, y=yc_s, line=dict(color='#1f77b4', width=2.5), name="Estável"))
-        fig.add_trace(go.Scatter(x=xc_f, y=yc_f, line=dict(color='black', width=1.2, dash='dash'), name="Falha"))
+        fig.add_trace(go.Scatter(x=xc_s, y=yc_s, line=dict(color='#1f77b4', width=2.5), name="Estado Estável"))
+        fig.add_trace(go.Scatter(x=xc_f, y=yc_f, line=dict(color='black', width=1.2, dash='dash'), name="Círculo (Falha)"))
         fig.add_trace(go.Scatter(x=p_x, y=p_y, line=dict(color='orange', width=1.5, dash='dot'), name="Trajetória"))
         fig.add_trace(go.Scatter(x=[sn_p], y=[tn_p], mode='markers', marker=dict(size=14, color='yellow' if falhou else 'lime', line=dict(width=2, color='black')), showlegend=False))
         
@@ -34,27 +40,22 @@ def plot_3d_block(params):
         reg = params.get('regime', 'Normal')
         s1, s3 = params.get('s1', 120.0), params.get('s3', 40.0)
         s2 = (s1 + s3) / 2
-        theta_rad = np.radians(a_s1)
-        
-        # Orientação por regime
+        theta_rad, merg_rad, lim = np.radians(a_s1), np.radians(90 - a_s1), 120 
+
         if reg == 'Normal': e1, e2, e3 = np.array([0,0,1]), np.array([1,0,0]), np.array([0,1,0])
         elif reg == 'Transcorrente': e1, e2, e3 = np.array([0,1,0]), np.array([0,0,1]), np.array([1,0,0])
         else: e1, e2, e3 = np.array([1,0,0]), np.array([0,1,0]), np.array([0,0,1])
 
-        # No Transcorrente, o ângulo theta gira em torno do eixo vertical (S2)
-        n_vec = e1 * np.cos(theta_rad) + e3 * np.sin(theta_rad)
-        f_dir = -e1 * np.sin(theta_rad) + e3 * np.cos(theta_rad)
+        f_dir, n_vec = -e1 * np.sin(merg_rad) + e3 * np.cos(merg_rad), e1 * np.cos(merg_rad) + e3 * np.sin(merg_rad)
         
         fig = go.Figure()
-        # Bloco
         v = np.array([[-40,-40,-50], [40,-40,-50], [40,40,-50], [-40,40,-50], [-40,-40,50], [40,-40,50], [40,40,50], [-40,40,50]])
         edges = [(0,1), (1,2), (2,3), (3,0), (4,5), (5,6), (6,7), (7,4), (0,4), (1,5), (2,6), (3,7)]
         for e in edges: fig.add_trace(go.Scatter3d(x=[v[e[0]][0], v[e[1]][0]], y=[v[e[0]][1], v[e[1]][1]], z=[v[e[0]][2], v[e[1]][2]], mode='lines', line=dict(color='black', width=2), showlegend=False))
 
-        # Plano (Giro em torno de S2)
-        sz = 45
+        sz = 42
         pts = np.array([-sz*e2-sz*f_dir, sz*e2-sz*f_dir, sz*e2+sz*f_dir, -sz*e2+sz*f_dir])
-        fig.add_trace(go.Mesh3d(x=pts[:,0], y=pts[:,1], z=pts[:,2], i=[0,0], j=[1,2], k=[2,3], color='lightblue', opacity=0.7))
+        fig.add_trace(go.Mesh3d(x=pts[:,0], y=pts[:,1], z=np.clip(pts[:,2], -50, 50), i=[0,0], j=[1,2], k=[2,3], color='lightblue', opacity=0.8))
 
         def add_arrow(direction, color, name, magnitude, inward=True):
             scale, d = 0.22, direction / (np.linalg.norm(direction) + 1e-9)
@@ -66,6 +67,6 @@ def plot_3d_block(params):
 
         add_arrow(e1, "blue", "S1", s1); add_arrow(e2, "green", "S2", s2); add_arrow(e3, "red", "S3", s3)
         add_arrow(n_vec, "black", "Sn", s1, False)
-        
+
         fig.update_layout(scene=dict(xaxis=dict(visible=False), yaxis=dict(visible=False), zaxis=dict(visible=False), aspectmode='cube'), height=500, margin=dict(l=0, r=0, t=0, b=0))
         st.plotly_chart(fig, use_container_width=True)
