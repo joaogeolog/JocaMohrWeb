a58b34c1efa0e4f3bd62d5b98a010629bcc31473
joaogeolog <joaogeolog@gmail.com>
2026-03-13 09:13:17 -0300

    Update interface_widgets.py

--- interface_widgets.py
+++ interface_widgets.py
@@ -24,29 +24,37 @@ def dual_input(label, min_v, max_v, key_p, step=1.0):
     return st.session_state[base_key]
 
 def render_bottom_interface():
-    """Renderiza os controles diretamente dentro da moldura."""
+    """Renderiza os controles com botões Reiniciar alinhados à direita dos títulos."""
     with st.container(border=True):
         c1, c2, c3 = st.columns(3)
+        
         with c1:
-            st.markdown("<b style='font-size:0.8em;'>1. TENSÕES (MPa)</b>", unsafe_allow_html=True)
-            if st.button("Reset Tensão"): reset_section(['s1', 's3', 'pp'])
+            hdr_col1, btn_col1 = st.columns([2, 1])
+            hdr_col1.markdown("<b style='font-size:0.8em;'>1. TENSÕES (MPa)</b>", unsafe_allow_html=True)
+            if btn_col1.button("Reiniciar", key="res_tens"): reset_section(['s1', 's3', 'pp'])
+            
             s1 = dual_input("S1", 0, 250, 's1')
             s3 = dual_input("S3", 0, 250, 's3')
             pp = dual_input("P. Poros", 0, 100, 'pp')
             alpha = st.slider("Biot (α)", 0.0, 1.0, float(DEFAULTS['alpha']), step=0.01, key='val_alpha')
             
         with c2:
-            st.markdown("<b style='font-size:0.8em;'>2. ROCHA</b>", unsafe_allow_html=True)
-            if st.button("Reset Rocha"): reset_section(['c', 'phi', 'ts', 'pc'])
+            hdr_col2, btn_col2 = st.columns([2, 1])
+            hdr_col2.markdown("<b style='font-size:0.8em;'>2. ROCHA</b>", unsafe_allow_html=True)
+            if btn_col2.button("Reiniciar", key="res_roc"): reset_section(['c', 'phi', 'ts', 'pc'])
+            
             c = dual_input("Coesão", 0, 100, 'c')
             phi = dual_input("Atrito (°)", 0, 90, 'phi')
             ts = dual_input("Tração", 0, 50, 'ts')
             pc = dual_input("Colapso", 0, 500, 'pc')
             
         with c3:
-            st.markdown("<b style='font-size:0.8em;'>3. PLANO</b>", unsafe_allow_html=True)
-            if st.button("Reset Plano"): reset_section(['ang'])
+            hdr_col3, btn_col3 = st.columns([2, 1])
+            hdr_col3.markdown("<b style='font-size:0.8em;'>3. PLANO</b>", unsafe_allow_html=True)
+            if btn_col3.button("Reiniciar", key="res_pla"): reset_section(['ang'])
+            
             regime = st.selectbox("Regime Tectônico", ["Normal", "Transcorrente", "Reverso"], index=0, key='regime_sel')
             ang = dual_input("Ang/S1", 0, 90, 'ang')
-            if st.button("Limpar Trajetória"): 
+            st.write("") # Espaçador
+            if st.button("Limpar Trajetória", use_container_width=True): 
                 st.session_state.path_x, st.session_state.path_y = [], []
