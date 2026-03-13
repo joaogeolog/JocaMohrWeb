523153e92644baba27e36fca1e30e4e34a655887
joaogeolog <joaogeolog@gmail.com>
2026-03-13 08:59:17 -0300

    Update visualizacao_plots.py

--- visualizacao_plots.py
+++ visualizacao_plots.py
@@ -7,22 +7,19 @@ def plot_mohr(x_env, y_env, xt_coll, xc_f, yc_f, res_c, xc_o, yc_o, sn_p, tn_p, 
     fig = go.Figure()
     fig.add_shape(type="line", x0=-50, y0=0, x1=250, y1=0, line=dict(color="black", width=2))
     
-    # Envoltórias
+    # Envoltórias, Círculos e Trajetória (Mantidos)
     fig.add_trace(go.Scatter(x=x_env[x_env<=0], y=y_env[x_env<=0], line=dict(color='blue', width=1.5), showlegend=False))
     fig.add_trace(go.Scatter(x=x_env[(x_env>0) & (x_env<=xt_coll)], y=y_env[(x_env>0) & (x_env<=xt_coll)], line=dict(color='red', width=1.5), showlegend=False))
     fig.add_trace(go.Scatter(x=x_env[x_env>xt_coll], y=y_env[x_env>xt_coll], line=dict(color='green', width=1.5), showlegend=False))
     
-    # Círculos
     m_s = (yc_o < res_c - 1e-3); m_f = ~m_s & (yc_o > 0.05)
     fig.add_trace(go.Scatter(x=np.where(m_s, xc_o, np.nan), y=np.where(m_s, yc_o, np.nan), line=dict(color='#1f77b4', width=2.5), name="Estável"))
     fig.add_trace(go.Scatter(x=np.where(~m_s, xc_o, np.nan), y=np.where(~m_s, yc_o, np.nan), line=dict(color='black', width=1, dash='dash'), name="Teórico"))
     
-    # Arcos de Falha
     fig.add_trace(go.Scatter(x=np.where(m_f & (xc_f < 0), xc_f, np.nan), y=np.where(m_f & (xc_f < 0), yc_f, np.nan), line=dict(color='blue', width=4), showlegend=False))
     fig.add_trace(go.Scatter(x=np.where(m_f & (xc_f >= 0) & (xc_f <= xt_coll), xc_f, np.nan), y=np.where(m_f & (xc_f >= 0) & (xc_f <= xt_coll), yc_f, np.nan), line=dict(color='red', width=4), showlegend=False))
     fig.add_trace(go.Scatter(x=np.where(m_f & (xc_f > xt_coll), xc_f, np.nan), y=np.where(m_f & (xc_f > xt_coll), yc_f, np.nan), line=dict(color='green', width=4), showlegend=False))
     
-    # Trajetória e Ponto
     fig.add_trace(go.Scatter(x=p_x, y=p_y, line=dict(color='orange', width=1.5, dash='dash'), name="Trajetória"))
     fig.add_trace(go.Scatter(x=[sn_p], y=[tn_p], mode='markers', marker=dict(size=14, color='yellow' if falhou else '#2ca02c', line=dict(width=2, color='black')), showlegend=False))
     
@@ -30,7 +27,7 @@ def plot_mohr(x_env, y_env, xt_coll, xc_f, yc_f, res_c, xc_o, yc_o, sn_p, tn_p, 
     st.plotly_chart(fig, use_container_width=True, config={'displayModeBar': False})
 
 def plot_3d_block(params):
-    """Visualização 3D com legendas persistentes independente da magnitude."""
+    """Visualização 3D com Contorno da Janela de Visualização."""
     ang_s1_deg = params['ang_s1']
     theta_rad = np.radians(ang_s1_deg)
     mergulho_rad = np.radians(90 - ang_s1_deg)
@@ -38,6 +35,7 @@ def plot_3d_block(params):
     reg = params['regime']
     s1, s3 = params['s1'], params['s3']
     s2 = (s1 + s3) / 2
+    lim = 120 # Define o limite da "caixa" de visualização
 
     if reg == 'Normal':
         e1, e2, e3 = np.array([0,0,1]), np.array([1,0,0]), np.array([0,1,0])
@@ -51,41 +49,44 @@ def plot_3d_block(params):
     
     fig = go.Figure()
 
-    # Cubo Wireframe
+    # 1. CONTORNO DA JANELA DE VISUALIZAÇÃO (Bounding Box)
+    # Desenha um cubo cinza claro nas extremidades do range [-lim, lim]
+    b = lim
+    box_v = np.array([[-b,-b,-b], [b,-b,-b], [b,b,-b], [-b,b,-b], [-b,-b,b], [b,-b,b], [b,b,b], [-b,b,b]])
+    box_edges = [(0,1), (1,2), (2,3), (3,0), (4,5), (5,6), (6,7), (7,4), (0,4), (1,5), (2,6), (3,7)]
+    for e in box_edges:
+        fig.add_trace(go.Scatter3d(x=[box_v[e[0]][0], box_v[e[1]][0]], y=[box_v[e[0]][1], box_v[e[1]][1]], z=[box_v[e[0]][2], box_v[e[1]][2]], 
+                                   mode='lines', line=dict(color='rgba(200,200,200,0.5)', width=1, dash='dash'), showlegend=False))
+
+    # 2. CUBO GEOLÓGICO (Wireframe interno)
     v = np.array([[-40,-40,-50], [40,-40,-50], [40,40,-50], [-40,40,-50], [-40,-40,50], [40,-40,50], [40,40,50], [-40,40,50]])
     edges = [(0,1), (1,2), (2,3), (3,0), (4,5), (5,6), (6,7), (7,4), (0,4), (1,5), (2,6), (3,7)]
     for e in edges:
-        fig.add_trace(go.Scatter3d(x=[v[e[0]][0], v[e[1]][0]], y=[v[e[0]][1], v[e[1]][1]], z=[v[e[0]][2], v[e[1]][2]], mode='lines', line=dict(color='black', width=2), showlegend=False))
+        fig.add_trace(go.Scatter3d(x=[v[e[0]][0], v[e[1]][0]], y=[v[e[0]][1], v[e[1]][1]], z=[v[e[0]][2], v[e[1]][2]], 
+                                   mode='lines', line=dict(color='black', width=2), showlegend=False))
 
-    # Plano azul
+    # Plano azul (Mesh3d)
     size = 42
     p1, p2 = -size * e2 - size * face_dir, size * e2 - size * face_dir
     p3, p4 = size * e2 + size * face_dir, -size * e2 + size * face_dir
     pts = np.array([p1, p2, p3, p4])
     fig.add_trace(go.Mesh3d(x=pts[:,0], y=pts[:,1], z=np.clip(pts[:,2], -50, 50), i=[0,0], j=[1,2], k=[2,3], color='lightblue', opacity=0.8, showlegend=False))
 
-    # Função para Vetores com correção de legenda persistente
     def add_arrow(direction, color, name, magnitude, inward=True):
         scale = 0.25
         d = direction / (np.linalg.norm(direction) + 1e-9)
-        
-        # Posição da flecha
         if inward:
             end_p, start_p, arrow_d = d * 55, d * (55 + magnitude * scale), -d
-            # Texto fica sempre um pouco além do início da flecha para não sumir
-            text_p = start_p + d * 15
+            text_p = start_p + d * 12
         else:
             start_p, end_p, arrow_d = np.array([0,0,0]), d * (magnitude * scale + 15), d
-            # Texto fica sempre um pouco além da ponta da flecha
-            text_p = end_p + d * 15
+            text_p = end_p + d * 12
 
         fig.add_trace(go.Scatter3d(x=[start_p[0], end_p[0]], y=[start_p[1], end_p[1]], z=[start_p[2], end_p[2]], mode='lines', line=dict(color=color, width=6), showlegend=False))
         fig.add_trace(go.Cone(x=[end_p[0]], y=[end_p[1]], z=[end_p[2]], u=[arrow_d[0]], v=[arrow_d[1]], w=[arrow_d[2]], colorscale=[[0, color], [1, color]], showscale=False, sizemode="absolute", sizeref=12))
-        
-        # O segredo: desabilitar a clipagem do texto e fixar posição relativa
         fig.add_trace(go.Scatter3d(x=[text_p[0]], y=[text_p[1]], z=[text_p[2]], mode='text', text=[f"<b>{name}</b>"], textfont=dict(color=color, size=14), showlegend=False))
 
-    # Tensões Principais
+    # Vetores
     add_arrow(e1, "blue", "S1", s1)
     add_arrow(e2, "green", "S2", s2)
     add_arrow(e3, "red", "S3", s3)
@@ -96,19 +97,13 @@ def plot_3d_block(params):
     if tau_val > 0.1:
         add_arrow(face_dir, "orange", "Tau", tau_val, False)
 
-    # Rotação Centralizada + Zoom Fiel
-    lim = 110 # Aumentei um pouco o limite para os nomes não serem cortados na borda da cena
     fig.update_layout(
         scene=dict(
             xaxis=dict(visible=False, range=[-lim, lim]),
             yaxis=dict(visible=False, range=[-lim, lim]),
             zaxis=dict(visible=False, range=[-lim, lim]),
             aspectmode='cube',
-            camera=dict(
-                eye=dict(x=1.1, y=1.1, z=1.1), 
-                center=dict(x=0, y=0, z=0), 
-                up=dict(x=0, y=0, z=1)
-            )
+            camera=dict(eye=dict(x=1.2, y=1.2, z=1.2), center=dict(x=0, y=0, z=0), up=dict(x=0, y=0, z=1))
         ), 
         height=500, margin=dict(l=0, r=0, t=0, b=0)
     )
