0d4eb787c331b9f5b4dff55c0ae9942ef56bed7e
joaogeolog <joaogeolog@gmail.com>
2026-03-13 09:20:19 -0300

    Update visualizacao_plots.py

--- visualizacao_plots.py
+++ visualizacao_plots.py
@@ -3,7 +3,7 @@ import streamlit as st
 import numpy as np
 
 def plot_mohr(x_env, y_env, xt_coll, xc_f, yc_f, res_c, xc_o, yc_o, sn_p, tn_p, p_x, p_y, falhou, params):
-    """Renderiza o Diagrama de Mohr envolto em uma moldura."""
+    """Renderiza o Diagrama de Mohr com eixos de Tensão Efetiva e Moldura."""
     with st.container(border=True):
         fig = go.Figure()
         fig.add_shape(type="line", x0=-50, y0=0, x1=250, y1=0, line=dict(color="black", width=2))
@@ -19,19 +19,17 @@ def plot_mohr(x_env, y_env, xt_coll, xc_f, yc_f, res_c, xc_o, yc_o, sn_p, tn_p, 
         fig.add_trace(go.Scatter(x=np.where(~m_s, xc_o, np.nan), y=np.where(~m_s, yc_o, np.nan), line=dict(color='black', width=1, dash='dash'), name="Teórico"))
         
         # Arcos de Falha
-        fig.add_trace(go.Scatter(x=np.where(m_f & (xc_f < 0), xc_f, np.nan), y=np.where(m_f & (xc_f < 0), yc_f, np.nan), line=dict(color='blue', width=4), showlegend=False))
-        fig.add_trace(go.Scatter(x=np.where(m_f & (xc_f >= 0) & (xc_f <= xt_coll), xc_f, np.nan), y=np.where(m_f & (xc_f >= 0) & (xc_f <= xt_coll), yc_f, np.nan), line=dict(color='red', width=4), showlegend=False))
-        fig.add_trace(go.Scatter(x=np.where(m_f & (xc_f > xt_coll), xc_f, np.nan), y=np.where(m_f & (xc_f > xt_coll), yc_f, np.nan), line=dict(color='green', width=4), showlegend=False))
+        for cond, c in [(m_f & (xc_f < 0), 'blue'), (m_f & (xc_f >= 0) & (xc_f <= xt_coll), 'red'), (m_f & (xc_f > xt_coll), 'green')]:
+            fig.add_trace(go.Scatter(x=np.where(cond, xc_f, np.nan), y=np.where(cond, yc_f, np.nan), line=dict(color=c, width=4), showlegend=False))
         
         # Trajetória e Ponto
         fig.add_trace(go.Scatter(x=p_x, y=p_y, line=dict(color='orange', width=1.5, dash='dash'), name="Trajetória"))
         fig.add_trace(go.Scatter(x=[sn_p], y=[tn_p], mode='markers', marker=dict(size=14, color='yellow' if falhou else '#2ca02c', line=dict(width=2, color='black')), showlegend=False))
         
         fig.update_layout(
-            xaxis=dict(range=[-50, 250], title="Sigma n (MPa)"),
-            yaxis=dict(range=[0, 100], scaleanchor="x", scaleratio=1, title="Tau (MPa)"),
-            template="plotly_white", height=500,
-            margin=dict(l=10, r=10, t=10, b=10),
+            xaxis=dict(range=[-50, 250], title="Tensão Normal Efetiva, σ'n (MPa)"),
+            yaxis=dict(range=[0, 100], scaleanchor="x", scaleratio=1, title="Tensão Cisalhante Efetiva, τ' (MPa)"),
+            template="plotly_white", height=500, margin=dict(l=10, r=10, t=10, b=10),
             legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1)
         )
         st.plotly_chart(fig, use_container_width=True, config={'displayModeBar': False})
@@ -39,69 +37,46 @@ def plot_mohr(x_env, y_env, xt_coll, xc_f, yc_f, res_c, xc_o, yc_o, sn_p, tn_p, 
 def plot_3d_block(params):
     """Visualização 3D com órbita travada, nomes persistentes e moldura."""
     with st.container(border=True):
-        theta_deg = params['ang_s1']
-        theta_rad = np.radians(theta_deg)
-        mergulho_rad = np.radians(90 - theta_deg)
-        
-        reg = params['regime']
-        s1, s3 = params['s1'], params['s3']
-        s2 = (s1 + s3) / 2
-        lim = 120 
+        ang_s1 = params['ang_s1']
+        theta_rad, mergulho_rad = np.radians(ang_s1), np.radians(90 - ang_s1)
+        reg, s1, s3 = params['regime'], params['s1'], params['s3']
+        s2, lim = (s1 + s3) / 2, 120 
 
-        if reg == 'Normal':
-            e1, e2, e3 = np.array([0,0,1]), np.array([1,0,0]), np.array([0,1,0])
-        elif reg == 'Transcorrente':
-            e1, e2, e3 = np.array([0,1,0]), np.array([0,0,1]), np.array([1,0,0])
-        else: # Reverso
-            e1, e2, e3 = np.array([1,0,0]), np.array([0,1,0]), np.array([0,0,1])
+        if reg == 'Normal': e1, e2, e3 = np.array([0,0,1]), np.array([1,0,0]), np.array([0,1,0])
+        elif reg == 'Transcorrente': e1, e2, e3 = np.array([0,1,0]), np.array([0,0,1]), np.array([1,0,0])
+        else: e1, e2, e3 = np.array([1,0,0]), np.array([0,1,0]), np.array([0,0,1])
 
         face_dir = -e1 * np.sin(mergulho_rad) + e3 * np.cos(mergulho_rad)
         norm_vec = e1 * np.cos(mergulho_rad) + e3 * np.sin(mergulho_rad)
-        
         fig = go.Figure()
 
-        # Bounding Box (Tracejada)
         b = lim
+        box_edges = [(0,1), (1,2), (2,3), (3,0), (4,5), (5,6), (6,7), (7,4), (0,4), (1,5), (2,6), (3,7)]
         box_v = np.array([[-b,-b,-b], [b,-b,-b], [b,b,-b], [-b,b,-b], [-b,-b,b], [b,-b,b], [b,b,b], [-b,b,b]])
-        for e in [(0,1), (1,2), (2,3), (3,0), (4,5), (5,6), (6,7), (7,4), (0,4), (1,5), (2,6), (3,7)]:
-            fig.add_trace(go.Scatter3d(x=[box_v[e[0]][0], box_v[e[1]][0]], y=[box_v[e[0]][1], box_v[e[1]][1]], z=[box_v[e[0]][2], box_v[e[1]][2]], 
-                                       mode='lines', line=dict(color='rgba(200,200,200,0.3)', width=1, dash='dash'), showlegend=False))
-
-        # Cubo Geológico
+        for e in box_edges:
+            fig.add_trace(go.Scatter3d(x=[box_v[e[0]][0], box_v[e[1]][0]], y=[box_v[e[0]][1], box_v[e[1]][1]], z=[box_v[e[0]][2], box_v[e[1]][2]], mode='lines', line=dict(color='rgba(200,200,200,0.3)', width=1, dash='dash'), showlegend=False))
+        
         v = np.array([[-40,-40,-50], [40,-40,-50], [40,40,-50], [-40,40,-50], [-40,-40,50], [40,-40,50], [40,40,50], [-40,40,50]])
-        for e in [(0,1), (1,2), (2,3), (3,0), (4,5), (5,6), (6,7), (7,4), (0,4), (1,5), (2,6), (3,7)]:
-            fig.add_trace(go.Scatter3d(x=[v[e[0]][0], v[e[1]][0]], y=[v[e[0]][1], v[e[1]][1]], z=[v[e[0]][2], v[e[1]][2]], 
-                                       mode='lines', line=dict(color='black', width=2), showlegend=False))
+        for e in box_edges:
+            fig.add_trace(go.Scatter3d(x=[v[e[0]][0], v[e[1]][0]], y=[v[e[0]][1], v[e[1]][1]], z=[v[e[0]][2], v[e[1]][2]], mode='lines', line=dict(color='black', width=2), showlegend=False))
 
-        # Plano Mesh3d
         size = 42
         pts = np.array([-size*e2-size*face_dir, size*e2-size*face_dir, size*e2+size*face_dir, -size*e2+size*face_dir])
         fig.add_trace(go.Mesh3d(x=pts[:,0], y=pts[:,1], z=np.clip(pts[:,2], -50, 50), i=[0,0], j=[1,2], k=[2,3], color='lightblue', opacity=0.8, showlegend=False))
 
-        def add_arrow(dir, col, name, mag, inward=True):
-            scale = 0.22 
-            d = dir / (np.linalg.norm(dir) + 1e-9)
-            if inward:
-                en_p, st_p, ar_d = d*55, d*(55+mag*scale), -d
-                offset = 8 if mag > 180 else 15
-                txt_p = st_p + d * offset
-            else:
-                st_p, en_p, ar_d = np.array([0,0,0]), d*(mag*scale+15), d
-                txt_p = en_p + d * 12
-            
-            fig.add_trace(go.Scatter3d(x=[st_p[0], en_p[0]], y=[st_p[1], en_p[1]], z=[st_p[2], en_p[2]], mode='lines', line=dict(color=col, width=6), showlegend=False))
-            fig.add_trace(go.Cone(x=[en_p[0]], y=[en_p[1]], z=[en_p[2]], u=[ar_d[0]], v=[ar_d[1]], w=[ar_d[2]], colorscale=[[0, col], [1, col]], showscale=False, sizemode="absolute", sizeref=12))
-            fig.add_trace(go.Scatter3d(x=[txt_p[0]], y=[txt_p[1]], z=[txt_p[2]], mode='text', text=[f"<b>{name}</b>"], textfont=dict(color=col, size=14), showlegend=False))
+        def add_arrow(direction, color, name, magnitude, inward=True):
+            scale, d = 0.22, direction / (np.linalg.norm(direction) + 1e-9)
+            if inward: en_p, st_p, ar_d, off = d*55, d*(55+magnitude*scale), -d, (8 if magnitude > 180 else 15)
+            else: st_p, en_p, ar_d, off = np.array([0,0,0]), d*(magnitude*scale+15), d, 12
+            fig.add_trace(go.Scatter3d(x=[st_p[0], en_p[0]], y=[st_p[1], en_p[1]], z=[st_p[2], en_p[2]], mode='lines', line=dict(color=color, width=6), showlegend=False))
+            fig.add_trace(go.Cone(x=[en_p[0]], y=[en_p[1]], z=[en_p[2]], u=[ar_d[0]], v=[ar_d[1]], w=[ar_d[2]], colorscale=[[0, color], [1, color]], showscale=False, sizemode="absolute", sizeref=12))
+            txt_p = (st_p if inward else en_p) + d * off
+            fig.add_trace(go.Scatter3d(x=[txt_p[0]], y=[txt_p[1]], z=[txt_p[2]], mode='text', text=[f"<b>{name}</b>"], textfont=dict(color=color, size=14), showlegend=False))
 
         add_arrow(e1, "blue", "S1", s1); add_arrow(e2, "green", "S2", s2); add_arrow(e3, "red", "S3", s3)
-        sn_val = s1*np.cos(theta_rad)**2 + s3*np.sin(theta_rad)**2
-        add_arrow(norm_vec, "black", "Sn", sn_val, False)
-        
+        add_arrow(norm_vec, "black", "Sn", s1*np.cos(theta_rad)**2 + s3*np.sin(theta_rad)**2, False)
         tau_val = abs(s1-s3)/2*np.sin(2*theta_rad)
-        if tau_val > 0.1:
-            add_arrow(face_dir, "orange", "Tau", tau_val, False)
+        if tau_val > 0.1: add_arrow(face_dir, "orange", "Tau", tau_val, False)
 
-        fig.update_layout(scene=dict(xaxis=dict(visible=False, range=[-lim, lim]), yaxis=dict(visible=False, range=[-lim, lim]), zaxis=dict(visible=False, range=[-lim, lim]), 
-                                     aspectmode='cube', camera=dict(eye=dict(x=1.1, y=1.1, z=1.1), center=dict(x=0, y=0, z=0))), 
-                          height=500, margin=dict(l=0, r=0, t=0, b=0))
+        fig.update_layout(scene=dict(xaxis=dict(visible=False, range=[-lim, lim]), yaxis=dict(visible=False, range=[-lim, lim]), zaxis=dict(visible=False, range=[-lim, lim]), aspectmode='cube', camera=dict(eye=dict(x=1.1, y=1.1, z=1.1), center=dict(x=0, y=0, z=0))), height=500, margin=dict(l=0, r=0, t=0, b=0))
         st.plotly_chart(fig, use_container_width=True, config={'displayModeBar': False})
